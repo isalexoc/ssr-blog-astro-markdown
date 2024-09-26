ce7435e03e287c07998b195be552820e4333f016
Isaac Orraiz <isaac87usa@gmail.com>
2024-09-26 13:03:19 -0400

    Isaac blog

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const SITE_TITLE = "TechPeople Blog";
-export const SITE_DESCRIPTION = "A blog about technology and people";
+export const SITE_TITLE = "Isaac's Blog";
+export const SITE_DESCRIPTION =
+  "I am Isaac Orraiz. A full stack developer based in virginia. This is my personal blog where I write about programming, technology, and other things that interest me.";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
